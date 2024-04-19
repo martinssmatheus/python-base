e3e18406cb3d5268cdfaed7f8233e02b75135847
matheus.martins <matheus.martins@nubbi.com.br>
2024-04-19 09:25:25 -0300

    cria hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python3
+"""Hello Word Multi Linguas.
 
-# Este programa imprime hello-world
+Dependendo da lingua configuada no ambiente o programa exibe a
+mensagem correspondente.
 
-print("Hello, World!!")
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./helloy.py
+"""
+__version__ = "0.0.1"
+__autor__ = "Matheus Martins"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
