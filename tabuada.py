c687c601424713efe523a57b2849bcf335b8069b
matheus.martins <matheus.martins@nubbi.com.br>
2024-04-23 17:57:51 -0300

    melhora a quebra de linha

--- tabuada.py
+++ tabuada.py
@@ -1,4 +1,4 @@
-#!/urs/bin/python
+#!/usr/bin/env python3
 
 """ Imprime a tabuada do 1 ao 10. """
 __version__ = "0.1.1"
@@ -9,8 +9,8 @@ __author__ = "Matheus"
 numeros = list(range(1, 11))
 
 for n1 in numeros:
-    print("{:-^18}".format(f"Tabuada do {n1}")+"\n")
+    print("\n"+"{:*^20}".format(f"Tabuada do {n1}"))
     for n2 in numeros:
         resultado = n1 * n2
-        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
-    print("#" * 18)
+        print("{:^20}".format(f"{n1} x {n2} = {resultado}"))
+    print("-" * 20)
