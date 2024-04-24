82bcdc565b82b0cb729dc001f801b1a048dfdc61
matheus.martins <matheus.martins@nubbi.com.br>
2024-04-24 18:11:35 -0300

    melhora escola.py, cria cadastro.py

---
+++ escola_v2_sets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Exibe relatório de crianças por atividade.
+
+Imprimir a lisa de crianças agrupadas por sala
+que frequentam cada uma das atividades.
+"""
+__version__ = "0.1.1"
+
+
+# DADOS
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+atividades = [
+    ("Ingles", aula_ingles),
+    ("Musica", aula_musica),
+    ("Dança", aula_danca),
+]
+
+# Listar alunos em cada atividade por sala
+for nome_atividade,  atividade in atividades:
+    print(f"\n**Alunos da atividade {nome_atividade}**")
+
+    # sala1 que tem interseção com a atividade
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2) & set(atividade)
+            
+    print("\nSala1 ", atividade_sala1)
+    print("\nSala2 ", atividade_sala2)
+    print("-" * 50)
